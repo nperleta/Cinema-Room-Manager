70db5239ca6679db0c7600e5c13501072ddd4e4d
nperleta <90847255+nperleta@users.noreply.github.com>
2022-01-06 11:47:40 +0100

    Add files via upload
    stage 1/5

--- CinemaRoomManager/src/cinemaRoomManager/Main.java
+++ CinemaRoomManager/src/cinemaRoomManager/Main.java
@@ -16,15 +16,15 @@ public class Main {
 
 		System.out.print("  ");
 
-		for (int rn = 1; rn < 9; rn++) {
+		for (int rn = 1; rn < 9; rn++) {                // rn = row numbers
 			System.out.print(rn + " ");
 		}
 
-		int cn = 0;
-		for (int r = 0; r < seats.length; r++) {
+		int cn = 0;                                     // cn = column numbers
+		for (int r = 0; r < seats.length; r++) {        // r = row (of S)
 			System.out.println();
 			System.out.print(++cn + " ");
-			for (int c = 0; c <= seats.length; c++) {
+			for (int c = 0; c <= seats.length; c++) {   // c = column(of S)
 				System.out.print(seats[r][c] + " ");
 			}
 		}
